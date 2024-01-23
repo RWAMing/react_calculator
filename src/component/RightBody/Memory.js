bf917d3f921d20cd3e27fe81abe4f488830469d4
RWAMing <art.rwam@gmail.com>
2024-01-24 06:34:14 +0900

    Feat: open nav when open_nav btn clicked

--- src/component/RightBody/Memory.js
+++ src/component/RightBody/Memory.js
@@ -5,5 +5,9 @@ import React, { useState, useEffect } from 'react';
 // Export
 export default function Memory() {
   // Return
-  return <ul className='body memory colunm'>메모리 공간</ul>;
+  return (
+    <ul className='body memory colunm'>
+      <li className='notice'>메모리에 저장된 내용이 없음</li>
+    </ul>
+  );
 }
