63d0d99520b37c1d07a1bdd60a2d683c9e651e0f
rwaming <art.rwam@gmail.com>
2024-03-13 13:47:00 +0900

    Comment: Write way to change for React, about History menu

--- src/component/History.jsx
+++ src/component/History.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Component
 import HistoryHead from './HistoryHead.jsx';
@@ -7,12 +7,52 @@ import Memory from './History/Memory.jsx';
 
 // Export
 export default function History() {
+  /*
+  1. 버튼 클릭 감지 = onclick
+  2. 무슨 버튼인지 확인
+  3. 해당 body로 바꾸기
+  */
+
+  const [menu, setMenu] = useState('log');
+
+  // Function
+  function changeMenu(clicked) {
+    const opendMenu = document.querySelector('.open');
+
+    // 해당 메뉴가 close 상태여야 함수 실행
+    if (opendMenu !== clicked.target) {
+      // 원래 열렸던 요소 숨기기
+      const opendName = String(opendMenu.classList).split(' ')[1]; // 클래스 이름 추출
+      const opendPage = document.querySelector(`.right .body.${opendName}`); // body 반환
+      opendPage.classList.add('disappear'); // body 안보이게 애니
+
+      // 0.2초 뒤
+      setTimeout(() => {
+        opendMenu.classList.remove('open'); // 메뉴 클래스 삭제
+        opendPage.style.display = 'none'; // body 보관
+        opendPage.classList.remove('disappear'); // body 애니 삭제
+
+        // 메뉴 open 변경
+        const openMenu = clicked.target;
+        openMenu.classList.add('open');
+
+        // 페이지 열기
+        const openName = String(openMenu.classList).split(' ')[1]; // 클래스 이름 추출
+        const openPage = document.querySelector(`.right .body.${openName}`); // body 반환
+        openPage.style.display = 'flex'; // body 노출 (애니는 useEffect가 해줌)
+
+        // useState 변경 (useEffect 실행)
+        setMenu(openName);
+      }, 200);
+    }
+  }
+
+  const [historyBody, setHistoryBody] = useState(<Log />);
   // Return
   return (
     <div className='colunm right'>
       <HistoryHead />
-      <Log />
-      <Memory />
+      {historyBody}
     </div>
   );
 }
