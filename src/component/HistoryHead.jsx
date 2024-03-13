039dd7565a4fcc1d81807902cd3b309597b617e0
rwaming <art.rwam@gmail.com>
2024-03-13 14:14:56 +0900

    Refactor: Arrange components: History, HistoryHead
    Related to: #12

--- src/component/HistoryHead.jsx
+++ src/component/HistoryHead.jsx
@@ -1,44 +1,34 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import changeMenu from './historyMenu';
 
-// Component
-import Log from './History/Log.jsx';
-import Memory from './History/Memory.jsx';
+/** Component - History 메뉴 버튼 */
+function HistoryMenu(props) {
+  const { name, className, states } = props;
 
-/**
- * @prop {Object} body - {state, set}
- */
+  return (
+    <button
+      type='button'
+      className={`button_menu ${className}`}
+      onClick={() => changeMenu(props)}>
+      {name}
+    </button>
+  );
+}
+
+/** Component - 헤더 우측(History) 메뉴 바 */
 export default function HistoryHead(props) {
   // Effect
-  // 마운트 : 메뉴 버튼 누르면 바뀌는 이벤트 리스너 심기
-  useEffect(() => {
-    document.querySelectorAll('.right .button_menu').forEach((button) => {
-      button.addEventListener('click', changeMenu);
-      return () => {
-        button.removeEventListener('click', changeMenu);
-      };
-    });
-    // 처음엔 기록 페이지 보여주기
-    document.querySelector('.right .body.log').style.display = 'flex';
-  }, []);
 
-  // 메뉴 변경시마다 : appear 애니메이션
-  useEffect(() => {
-    document.querySelector(`.right .body.${menu}`).classList.add('appear');
-  }, [menu]);
+  // // 메뉴 변경시마다 : appear 애니메이션
+  // useEffect(() => {
+  //   document.querySelector(`.right .body.${menu}`).classList.add('appear');
+  // }, [menu]);
 
   // Return
   return (
     <div className='head'>
-      <button type='button' className='button_menu log open'>
-        기록
-      </button>
-      <button type='button' className='button_menu memory'>
-        메모리
-      </button>
+      <HistoryMenu name='기록' className='log' states={props} />
+      <HistoryMenu name='메모리' className='memory' states={props} />
     </div>
   );
 }
